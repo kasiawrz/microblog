3c5fdbab01ea23803577a0118c85000b91beccba
Kasia Wrzesniak <kasia.wrzesniak@trivago.com>
2020-05-17 18:23:46 +0200

    add React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
